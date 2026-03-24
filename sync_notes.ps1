ae79561547627fee5487869cd219157ace6adad1
Gr1ybt <mky3329395725@gmail.com>
2026-03-24 18:25:44 +0800

    添加可用的同步Notes仓库方式

---
+++ sync_notes.ps1
@@ -0,0 +1,10 @@
+﻿$Source = "D:\17Obsidian\20260306\大二下学期\02 CS61A"
+$Destination = "D:\01班级文件与作业\07_CS61A\Notes"
+
+if (!(Test-Path $Destination)) {
+    New-Item -ItemType Directory -Path $Destination
+}
+
+robocopy $Source $Destination /MIR /R:0 /W:0 /MT:32
+
+Write-Host "✅ 同步完成！现在可以执行 git add 了。" -ForegroundColor Green
