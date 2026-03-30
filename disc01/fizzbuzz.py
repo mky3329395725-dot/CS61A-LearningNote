69e105b9d40ebee50aa69f0add3396f051058b12
Gr1ybt <mky3329395725@gmail.com>
2026-03-30 16:57:29 +0800

    Correct the answer of 2 hws of disc01, especially fizzbuzz.py, I forgot writing doctest for it, and I was wrong in using '%'.Finished and test passed.

--- disc01/fizzbuzz.py
+++ disc01/fizzbuzz.py
@@ -1,12 +1,36 @@
 def fizzbuzz(n):
+    """Print all numbers lower than n and greater than 0,
+    but print 'fizz' when the number is divisible by 3,
+    print 'buzz' when it is divisible by 5,
+    print 'fizzbuzz' when it is divisible by both 3 and 5
+    >>>result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> print(result)
+    None"""
     i = 1
     while(i <= n):
-        if (n % i == 3 and n % i == 5):
+        if (i % 3 == 0 and i % 5 == 0):
             print("fizzbuzz")
-        elif (n % i == 3):
+        elif (i % 3 == 0):
             print("fizz")
-        elif (n % i == 5):
+        elif (i % 5 == 0):
             print("buzz")
         else:
             print(i)
+        i += 1
     return None
--- disc01/is_prime.py
+++ disc01/is_prime.py
@@ -13,6 +13,6 @@ def is_prime(n):
     i = 2
     while (i < n):
         if (n % i == 0):
-            return True
+            return False
         i += 1
-    return False
+    return True
